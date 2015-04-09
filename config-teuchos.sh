37041b972cf9da6dcca69131df9157fefd3433ef
Ondřej Čertík <ondrej.certik@gmail.com>
2015-04-09 16:40:42 -0600

    BUILD Enable Debug

--- config-teuchos.sh
+++ config-teuchos.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 
 cmake \
+    -D CMAKE_BUILD_TYPE:STRING=Debug \
     -D Trilinos_ASSERT_MISSING_PACKAGES=OFF \
     -D Trilinos_ENABLE_TeuchosCore=ON \
     -D Trilinos_ENABLE_DEBUG=ON \
