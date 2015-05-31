7db9feaddb1680507649ae947eb563cd00fab353
Ondřej Čertík <ondrej.certik@gmail.com>
2015-05-31 17:43:58 -0600

    BUILD Install and add release config

--- config-teuchos-release.sh
+++ config-teuchos-release.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
 
 cmake \
-    -D CMAKE_BUILD_TYPE:STRING=Debug \
+    -D CMAKE_BUILD_TYPE:STRING=Release \
     -D Trilinos_ASSERT_MISSING_PACKAGES=OFF \
     -D Trilinos_ENABLE_TeuchosCore=ON \
-    -D Trilinos_ENABLE_DEBUG=ON \
+    -D Trilinos_ENABLE_DEBUG=OFF \
     -D Trilinos_ENABLE_TESTS=ON \
     -D CMAKE_INSTALL_PREFIX:PATH="xx" \
     ..
